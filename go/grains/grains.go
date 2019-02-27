d1cfdc63c162517f93a8546ed73a6e072421ddf8
Darren Lester <darren@darrenlester.com>
2019-02-27 22:49:19 +0000

    Use left shift operator

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -7,19 +7,10 @@ func Square(square int) (uint64, error) {
 	if square < 1 || square > 64 {
 		return 0, errors.New("square must be in range [1,64]")
 	}
-	grains := 1
-	for i := 1; i < square; i++ {
-		grains *= 2
-	}
-	return uint64(grains), nil
+	return 1 << uint(square-1), nil
 }
 
 // Total returns the total number of grains on the board
 func Total() uint64 {
-	var total uint64
-	for i := 1; i <= 64; i++ {
-		n, _ := Square(i)
-		total += n
-	}
-	return total
+	return 1<<64 - 1
 }
